d07d6160bdbc93c8ec90004a05029aa876790df0
MadeWithMike <mikeobilade@gmail.com>
2023-04-09 12:01:40 -0500

    add all

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+let multer = require('multer')
+let upload = multer({dest: './public/uploaded'})
 
 var app = express();
 
@@ -11,10 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(request, response) {
+  console.log(request.file, "request.file")
+ // console.log(request.body, "request.body")
+  let obj = {name: request.file.originalname, type: request.file.mimetype, size: request.file.size}
+
+ // console.log(response.json(obj), "json response")
+
+  response.json(obj)
+})
